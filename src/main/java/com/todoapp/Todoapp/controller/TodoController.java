df806467d345873f3687e3322c187335decb908a
Keerthi <rhkeerthi3@gmail.com>
2025-07-07 20:19:23 +0530

    modified controller class

--- src/main/java/com/todoapp/Todoapp/controller/TodoController.java
+++ src/main/java/com/todoapp/Todoapp/controller/TodoController.java
@@ -1,4 +1,16 @@
 package com.todoapp.Todoapp.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TodoController {
+
+
+    @GetMapping({"","/","/home"})
+    public String showHomePage(){
+        return "index";
+
+    }
 }
