207109f00821cf0a34d4bc6576630fca40bdb70d
Keerthi <rhkeerthi3@gmail.com>
2025-07-07 22:58:59 +0530

    modified todocontroller

--- src/main/java/com/todoapp/Todoapp/controller/TodoController.java
+++ src/main/java/com/todoapp/Todoapp/controller/TodoController.java
@@ -1,33 +1,26 @@
 package com.todoapp.Todoapp.controller;
 
-
 import com.todoapp.Todoapp.entity.TodoEntity;
 import com.todoapp.Todoapp.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
 public class TodoController {
 
-   private  final TodoRepository todoRepository;
-    @GetMapping({"","/","/home"})
-    public String showHomePage(Model model ){
+    private final TodoRepository todoRepository;
 
-        model.addAttribute("todos",todoRepository.findAll());
-
-        todoRepository.findAll();
+    @GetMapping({"", "/", "/home"})
+    public String showHomePage(Model model) {
+        model.addAttribute("todos", todoRepository.findAll());
         return "index";
-
     }
 
-    @PostMapping
-    public String add(@RequestParam String title){
+    @PostMapping("/add")
+    public String add(@RequestParam String title) {
         TodoEntity newTodo = TodoEntity.builder()
                 .title(title)
                 .completed(false)
@@ -37,21 +30,19 @@ public class TodoController {
     }
 
     @GetMapping("/update/{id}")
-    public  String update(@PathVariable Long id){
-        TodoEntity existingTable= todoRepository.findById(id)
-                .orElseGet(()-> new RuntimeException("Todo not found:"+id));
-
-        existingTable.setCompleted(true);
-        todoRepository.save(existingTable);
+    public String update(@PathVariable Long id) {
+        TodoEntity existingTodo = todoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Todo not found: " + id));
+        existingTodo.setCompleted(true);
+        todoRepository.save(existingTodo);
         return "redirect:/";
     }
 
     @GetMapping("/delete/{id}")
-    public String delete(@PathVariable Long id){
-        TodoEntity existingTable= todoRepository.findById(id)
-                .orElseGet(()-> new RuntimeException("Todo not found:"+id));
-        todoRepository.delete(existingTable);
-        return  "redirect:/";
-
+    public String delete(@PathVariable Long id) {
+        TodoEntity existingTodo = todoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Todo not found: " + id));
+        todoRepository.delete(existingTodo);
+        return "redirect:/";
     }
 }
