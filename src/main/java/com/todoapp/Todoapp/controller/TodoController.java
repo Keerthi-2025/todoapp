c5e893c311a3012a73eaaf7cef87b2fad803a040
Keerthi <rhkeerthi3@gmail.com>
2025-07-07 21:44:35 +0530

    modified todocontroller and index.html

--- src/main/java/com/todoapp/Todoapp/controller/TodoController.java
+++ src/main/java/com/todoapp/Todoapp/controller/TodoController.java
@@ -1,16 +1,37 @@
 package com.todoapp.Todoapp.controller;
 
 
+import com.todoapp.Todoapp.entity.TodoEntity;
+import com.todoapp.Todoapp.repository.TodoRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor
 public class TodoController {
 
-
+   private  final TodoRepository todoRepository;
     @GetMapping({"","/","/home"})
-    public String showHomePage(){
+    public String showHomePage(Model model ){
+
+        model.addAttribute("todos",todoRepository.findAll());
+
+        todoRepository.findAll();
         return "index";
 
+    }
+
+    @PostMapping
+    public String add(@RequestParam String title){
+        TodoEntity newTodo = TodoEntity.builder()
+                .title(title)
+                .completed(false)
+                .build();
+        todoRepository.save(newTodo);
+        return "redirect:/";
     }
 }
